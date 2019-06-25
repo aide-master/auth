7be5061eb297836a75d841a5f2c8440f0be6eb9f
Yudu Ban <banyudu@gmail.com>
2019-06-25 20:19:27 +0800

    add login with github button

---
+++ packages/frontend/src/providers/github.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ID = 'd7932611c30ea08bd0c8'
+export const REDIRECT_URI = 'https://authorize.aidemaster.com/callback/github'
+export const STATE = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+export const AUTH_LINK = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}`
