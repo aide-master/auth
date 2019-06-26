c884e101ffc4cf9a2e1bfcd52c893579a1541ea6
Yudu Ban <banyudu@gmail.com>
2019-06-26 14:25:00 +0800

    call api in github callback page

--- packages/frontend/src/callbacks/github/index.tsx
+++ packages/frontend/src/callbacks/github/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useReactRouter from 'use-react-router'
+import { login } from '../../providers/github'
 import './github.css'
 
 const GithubCallback: React.FC = () => {
+  const { location } = useReactRouter()
+  useEffect(() => {
+    const params = new URLSearchParams(location.search)
+    const code = params.get('code') || ''
+    login(code)
+  }, [location.search])
   return (
     <div className='Github'>
       github callback page
