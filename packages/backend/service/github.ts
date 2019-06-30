bebc736a39c82078b87b8bf964439389c50a2f64
Yudu Ban <banyudu@icloud.com>
2019-06-30 12:55:33 +0800

    use formdata

--- packages/backend/service/github.ts
+++ packages/backend/service/github.ts
@@ -1,15 +1,16 @@
 import axios from 'axios'
+import * as FormData from 'form-data'
 
 const REDIRECT_URI = 'https://auth.aidemaster.com/callback/github'
 
 export async function getAccessToken (code: string, state: string) {
-  const res = await axios.post('https://github.com/login/oauth/access_token', {
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_CLIENT_SECRET,
-    code,
-    state,
-    redirect_uri: REDIRECT_URI
-  })
+  const data = new FormData()
+  data.append('client_id', process.env.GITHUB_CLIENT_ID || '')
+  data.append('client_secret', process.env.GITHUB_CLIENT_SECRET || '')
+  data.append('code', code)
+  data.append('state', state)
+  data.append('redirect_uri', REDIRECT_URI)
+  const res = await axios.post('https://github.com/login/oauth/access_token', data)
   const responseData = res.data
   return {
     token: responseData.access_token,
