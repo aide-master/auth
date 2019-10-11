cc5cbee69b3d0ce0423f869fed9d93f33fdaf514
Yudu Ban <banyudu@gmail.com>
2019-10-11 21:24:49 +0800

    fix: set Authorization header in rest methods

--- packages/frontend/src/common/rest.ts
+++ packages/frontend/src/common/rest.ts
@@ -2,34 +2,34 @@ import axios, { AxiosRequestConfig } from 'axios'
 import * as utils from '../common/utils'
 
 function getAuthHeader () {
-  const token = utils.getToken()
+  const token = utils.getToken() || ''
   return { Authorization: token }
 }
 
 export async function get (url: string, config?: AxiosRequestConfig) {
   const options = Object.assign({}, config, {
-    header: getAuthHeader()
+    headers: getAuthHeader()
   })
   return axios.get(url, options)
 }
 
 export async function post (url: string, data: any, config?: AxiosRequestConfig) {
   const options = Object.assign({}, config, {
-    header: getAuthHeader()
+    headers: getAuthHeader()
   })
   return axios.post(url, data, options)
 }
 
 export async function put (url: string, data: any, config?: AxiosRequestConfig) {
   const options = Object.assign({}, config, {
-    header: getAuthHeader()
+    headers: getAuthHeader()
   })
   return axios.put(url, data, options)
 }
 
 export async function del (url: string, config?: AxiosRequestConfig) {
   const options = Object.assign({}, config, {
-    header: getAuthHeader()
+    headers: getAuthHeader()
   })
   return axios.delete(url, options)
 }
