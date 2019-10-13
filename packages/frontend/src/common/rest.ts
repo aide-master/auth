fa996d2a425fc0f8b15de7f056762c1fe80d5bb2
Yudu Ban <banyudu@gmail.com>
2019-10-13 21:22:08 +0800

    fix: use withCredentials in rest.ts

--- packages/frontend/src/common/rest.ts
+++ packages/frontend/src/common/rest.ts
@@ -6,30 +6,26 @@ function getAuthHeader () {
   return { Authorization: token }
 }
 
-export async function get (url: string, config?: AxiosRequestConfig) {
-  const options = Object.assign({}, config, {
-    headers: getAuthHeader()
+function getOptions (config?: AxiosRequestConfig) {
+  const options: AxiosRequestConfig = Object.assign({}, config, {
+    headers: getAuthHeader(),
+    withCredentials: true
   })
-  return axios.get(url, options)
+  return options
+}
+
+export async function get (url: string, config?: AxiosRequestConfig) {
+  return axios.get(url, getOptions(config))
 }
 
 export async function post (url: string, data: any, config?: AxiosRequestConfig) {
-  const options = Object.assign({}, config, {
-    headers: getAuthHeader()
-  })
-  return axios.post(url, data, options)
+  return axios.post(url, data, getOptions(config))
 }
 
 export async function put (url: string, data: any, config?: AxiosRequestConfig) {
-  const options = Object.assign({}, config, {
-    headers: getAuthHeader()
-  })
-  return axios.put(url, data, options)
+  return axios.put(url, data, getOptions(config))
 }
 
 export async function del (url: string, config?: AxiosRequestConfig) {
-  const options = Object.assign({}, config, {
-    headers: getAuthHeader()
-  })
-  return axios.delete(url, options)
+  return axios.delete(url, getOptions(config))
 }
